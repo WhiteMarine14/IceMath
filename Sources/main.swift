026306cb7b19f063e743ee34ab0f2b99a0e1153b
WhiteMarine14 <193433178+WhiteMarine14@users.noreply.github.com>
2025-04-13 14:58:01 +0200

    Add operation
    Add case sum

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,5 +3,9 @@ import Foundation;
 print("IceMath")
 print("-------")
 func Calculate(num1: Double, num2: Double, operation:String) -> Double? {
+    switch operation{
+     case "+": 
+        return num1+num2
     
+    }
 }
