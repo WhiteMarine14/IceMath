9783738cff8b03d25a21288603fa9fe2523b85aa
WhiteMarine14 <193433178+WhiteMarine14@users.noreply.github.com>
2025-04-13 15:01:12 +0200

    Add case subtraction

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,6 +6,8 @@ func Calculate(num1: Double, num2: Double, operation:String) -> Double? {
     switch operation{
      case "+": 
         return num1+num2
+    case "-":
+        return num1-num2
     
     }
 }
